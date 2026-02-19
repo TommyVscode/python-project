a405c768d76a76fe282ffbaa905b1204e46eb242
MTiTommy <mohammadt.tamim@gmail.com>
2026-02-19 16:06:51 +0600

    Tkiner_windows.py

---
+++ windows.py
@@ -0,0 +1,10 @@
+from tkinter import*
+#initial the tk
+root = Tk()
+# start a mainloop
+
+# creating a label widget
+label =Label(root,text="Hello World")
+# for showing in display or root
+label.pack()
+root.mainloop()
